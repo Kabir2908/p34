02580721cb538f43cc44b93f92ecaa68b9a09afd
Kabir2908 <68494610+Kabir2908@users.noreply.github.com>
2020-12-16 14:18:46 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -11,8 +11,9 @@ function preload()
 function setup() {
   database = firebase.database();
 	createCanvas(500, 500);
-  dog = createSprite(250,450);
+  dog = createSprite(250,250);
   dog.addImage(dogImg);
+  dog.scale = 0.25;
   foodStock = database.ref('food');
   foodStock.on("value",readStock);
 }
@@ -22,7 +23,7 @@ function draw() {
 background(46,139,87);
 if(foodS!==undefined){
   if(keyWentDown(UP_ARROW)){
-    writeStock(foodS);
+    writeStock(20);
     dog.addImage(dogHappyImg);
   }
 }
@@ -30,8 +31,8 @@ if(foodS!==undefined){
   drawSprites();
   //add styles here
   textSize(20);
-  text("Remaining Milk: "+foodS,250,380)
-  text("Note: Press Up Arrow Key to feed Tom Milk",250,350);
+  text("Remaining Milk: "+foodS,150,380)
+  text("Note: Press Up Arrow Key to feed Tom Milk",100,350);
 }
 
 function readStock(data){
